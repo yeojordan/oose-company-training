6ebf14366dd1a5bdb99a1a871d15059b6a053f59
Jordan Yeo <jordanyeo23@gmail.com>
2016-10-04 16:26:24 +0800

    Store command line params

--- src/Start.java
+++ src/Start.java
@@ -7,11 +7,21 @@ public class Start
 {
     public static void main(String[] args)
     {
+        if ( args.length != 5)
+        {
+            throw new IllegalArgumentException("Incorrect number of command line args");
+        }
 
+        String propertyFile = args[0];
+        String eventFile    = args[1];
+        String planFile     = args[2];
+        int startYear       = Integer.parseInt(args[3]);
+        int endYear         = Integer.parseInt(args[4]);;
+        
         SimulatorController controller = new SimulatorController();
 
         FileReading fileReader = new ReadProperty(controller);
-        
+
         fileReader.read("propertyfile.txt");
 
         controller.printProperties();
